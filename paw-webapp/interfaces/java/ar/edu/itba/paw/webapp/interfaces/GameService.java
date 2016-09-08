1e82ec35d8f4eefe52f5a79f32821fc1d8560065
Juan Li Puma <juanlipuma94@gmail.com>
2016-09-07 22:03:30 -0300

    Advance with the guide
    - Configured basic game service and implementations

---
+++ paw-webapp/interfaces/java/ar/edu/itba/paw/webapp/interfaces/GameService.java
@@ -0,0 +1,7 @@
+package ar.edu.itba.paw.webapp.interfaces;
+
+public interface GameService {
+
+    //TODO change Object to Game, rebase with master branch
+    Object findByName(String name);
+}
--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,15 +1,21 @@
 package ar.edu.itba.paw.webapp.controller;
 
-import org.springframework.context.annotation.Bean;
+import ar.edu.itba.paw.webapp.interfaces.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Controller
 public class HelloWorldController {
+
+    private final GameService gameService;
+
+    @Autowired
+    public HelloWorldController(GameService gameService) {
+        //Spring is in charge of providing the gameService parameter.
+        this.gameService = gameService;
+    }
 
     @RequestMapping("/")
     public ModelAndView helloWorld() {
