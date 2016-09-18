b48664135c856535181a65e02dd5871df2c81e68
Juan Li Puma <juanlipuma94@gmail.com>
2016-09-18 01:22:31 -0300

    Use game ID to search information about a specific game in Game page
    - Only display release year in Search page
    - Document interface methods
    - Return NULL if findById() finds no games

--- paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/webapp/interfaces/GameService.java
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/webapp/interfaces/GameService.java
@@ -8,6 +8,20 @@ import java.util.List;
 
 public interface GameService {
 
+    /**
+     * Finds all games with matching names that meet the criteria specified in {@code filters}.
+     *
+     * @param name Partial or complete name of the game. An empty name will match all games.
+     * @param filters Criteria for the games to match.
+     * @return Games matching the specified criteria. TODO is there a maximum list size? If so, specify. If not, specify.
+     */
     List<Game> searchGames(String name, Collection<Filter> filters);
+
+    /**
+     * Finds the game with the specified ID.
+     *
+     * @param id The game ID.
+     * @return The matching game, or {@code null} if not found.
+     */
     Game findById(int id);
 }
