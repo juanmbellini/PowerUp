e874d383895319ecf6e933f49d0743d618183253
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-18 03:30:40 -0300

    Modify gameSearch method in persistence module
    - Now it receives a Map<FilterType, List<String>> map
    - Now it makes one join per FilterType
    - Now it makes ORs

--- paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/webapp/interfaces/GameDao.java
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/webapp/interfaces/GameDao.java
@@ -1,11 +1,14 @@
 package ar.edu.itba.paw.webapp.interfaces;
 
 import ar.edu.itba.paw.webapp.model.Filter;
+import ar.edu.itba.paw.webapp.model.FilterCategory;
 import ar.edu.itba.paw.webapp.model.Game;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public interface GameDao {
 
-    Collection<Game> searchGame(String name, Collection<Filter> filters);
+    Collection<Game> searchGame(String name, Map<FilterCategory,List<String>> filters);
 }
--- paw-webapp/persistence/src/main/java/ar/edu/itba/paw/webapp/persistence/GameJdbcDao.java
+++ paw-webapp/persistence/src/main/java/ar/edu/itba/paw/webapp/persistence/GameJdbcDao.java
@@ -2,7 +2,9 @@ package ar.edu.itba.paw.webapp.persistence;
 
 import ar.edu.itba.paw.webapp.interfaces.GameDao;
 import ar.edu.itba.paw.webapp.model.Filter;
+import ar.edu.itba.paw.webapp.model.FilterCategory;
 import ar.edu.itba.paw.webapp.model.Game;
+import org.atteo.evo.inflector.English;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowCallbackHandler;
@@ -31,57 +33,95 @@ public class GameJdbcDao implements GameDao {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public Collection<Game> searchGame(String name, Collection<Filter> filters) {
-        StringBuilder nameLike = new StringBuilder('%');
-        name.replace(' ', '%');
-        nameLike.append(name).append('%');
-        ArrayList<Game> gameList = new ArrayList();
-        System.out.println(filters.size());
-        Object[] parameters = new Object[filters.size() + 1];
-        parameters[0] = nameLike.toString();
-        int i = 1;
-        StringBuilder searchString = new StringBuilder("SELECT power_up.games.name, avg_score, summary, power_up.platforms.name FROM power_up.games, power_up.game_platforms, power_up.platforms");
-        StringBuilder whereSentence = new StringBuilder(" WHERE LOWER(power_up.games.name) LIKE LOWER(?) AND power_up.game_platforms.game_id = power_up.games.id AND " +
-                "power_up.game_platforms.platform_id = power_up.platforms.id");
+
+
+    public Collection<Game> searchGame(String name, Map<FilterCategory, List<String>> filters) {
+
+        int filtersAmount = countFilters(filters);
+        String[] parameters = new String[(filtersAmount * 2) + 1];
+        parameters[0] = name;
+
+        String tablesString = "SELECT power_up.games.name, avg_score, summary, power_up.platforms.name" +
+                " FROM power_up.games" +
+                " INNER JOIN power_up.game_platforms ON power_up.games.id = power_up.game_platforms.game_id" +
+                " INNER JOIN power_up.platforms ON power_up.game_platforms.platform_id = power_up.platforms.id";
+        String nameString = "WHERE LOWER(power_up.games.name) like %LOWER(?)%";
+        String filtersString = "";
+
 
         //Joins with specific table if a filter of that table is needed
-        for (Filter filter : filters) {
-            //Join on relationship table
-            searchString.append(", power_up.game_").append(filter.getType().name()).append(" AS  game_").append(filter.getType().name()).append("_").append(filter.getName());
-            whereSentence.append(" AND power_up.games.id = game_").append(filter.getType().name()).append("_").append(filter.getName()).append(".game_id");
-            //Join on entity table;
-            if ((filter.getType() != Filter.FilterCategory.DEVELOPERS) && (filter.getType() != Filter.FilterCategory.PUBLISHERS)) {
-                searchString.append(", power_up.").append(filter.getType().name()).append(" AS ").append(filter.getType().name()).append("_").append(filter.getName());
-                whereSentence.append(" AND game_").append(filter.getType().name()).append("_").append(filter.getName()).append(".").append(filter.getType().name().substring(0, filter.getType().name().length() - 1)).append("_id = ").append(filter.getType().name()).append("_").append(filter.getName()).append(".id");
-                whereSentence.append(" AND LOWER(").append(filter.getType().name()).append("_").append(filter.getName()).append(".name) = LOWER(?)");
-                parameters[i] = filter.getName();
-                i++;
-            } else {
-                searchString.append(", power_up.companies AS companies_").append(filter.getType()).append("_").append(filter.getName());
-                whereSentence.append(" AND game_").append(filter.getType()).append("_").append(filter.getName()).append(".").append(filter.getType().name().substring(0, filter.getType().name().length() - 1)).append("_id = companies_").append(filter.getType().name()).append("_").append(filter.getName()).append(".id");
-                whereSentence.append(" AND LOWER(companies_").append(filter.getType()).append("_").append(filter.getName()).append(".name) = LOWER(?)");
-                parameters[i] = filter.getName();
-                i++;
+        int parameterCount = 1;
+        for (FilterCategory filter : filters.keySet()) {
+            // table "platforms" is already joined
+            if (!filter.equals(FilterCategory.platform)) {
+                tablesString += " " + createJoinSentence(filter);
             }
-
+            filtersString += " AND ( ";
+            List<String> values = filters.get(filter);
+            int valuesCount = 0;
+            for (String value : values) {
+                if (valuesCount > 0) {
+                    filtersString += " OR ";
+                }
+                filtersString += createFilterSentence(filter);
+                parameters[parameterCount] = value;
+                parameters[parameterCount + filtersAmount] = Integer.toString(values.size());
+                parameterCount++;
+                valuesCount++;
+            }
+            filtersString += " )";
 
         }
-        searchString.append(whereSentence);
-        System.out.println(searchString.toString().toLowerCase());
-        jdbcTemplate.query(searchString.toString().toLowerCase(), parameters, new RowCallbackHandler() {
+        String query = tablesString + " " + nameString + filtersString;
+        List<Game> gameList = new ArrayList();
+        jdbcTemplate.query(query.toString().toLowerCase(), parameters, new RowCallbackHandler() {
 
-                    @Override
-                    public void processRow(ResultSet rs) throws SQLException {
-                        Game newGame = new Game();
-                        newGame.setName(rs.getString("name"));
-                        newGame.setSummary(rs.getString("summary"));
-                        gameList.add(newGame);
-                    }
-                }
-
-
-        );
+            @Override
+            public void processRow(ResultSet rs) throws SQLException {
+                Game newGame = new Game();
+                newGame.setName(rs.getString("name"));
+                newGame.setSummary(rs.getString("summary"));
+                gameList.add(newGame);
+            }
+        });
         return gameList;
     }
+
+
+
+    private String createJoinSentence (FilterCategory filter) {
+
+        boolean useCompany = filter.equals(FilterCategory.developer) || filter.equals(FilterCategory.platform);
+        String filterName = filter.name();
+        String entityTable = "power_up." + (useCompany ? "companies": English.plural(filterName));
+        String relationTable = "power_up.game_" + English.plural(filterName);
+
+        String sentence = "INNER JOIN " + relationTable + " ON power_up.games.id = " + relationTable + ".game_id";
+        sentence += " INNER JOIN " + entityTable
+                + " ON " + relationTable + "." + filterName + "_id = " + entityTable + ".id";
+        return sentence;
+    }
+
+    private String createFilterSentence(FilterCategory filter) {
+        boolean useCompany = filter.equals(FilterCategory.developer) || filter.equals(FilterCategory.platform);
+        String entityTable = "power_up." + (useCompany ? "companies": English.plural(filter.name()));
+
+        return "LOWER(" + entityTable + ".name) = LOWER(?)";
+    }
+
+
+
+
+    private int countFilters(Map<FilterCategory, List<String>> filters) {
+        int count = 0;
+        for (List<String> list : filters.values()) {
+            count += list.size();
+        }
+        return count;
+    }
+
+
+
+
 }
 
