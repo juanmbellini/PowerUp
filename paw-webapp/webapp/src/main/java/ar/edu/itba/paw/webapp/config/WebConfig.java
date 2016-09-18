87bb8b466f7caca26ca809c439c436f1048a2e81
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-18 04:19:55 -0300

    Change postgres user and password
    - User is postgres and password is paw
    - Also changed getDataSource() method name to dataSource()

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -28,15 +29,16 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public DataSource getDataSource() {
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("jorge");
+        dataSource.setPassword("paw");
 
         return dataSource;
     }
+
 
     //MAGIA! http://stackoverflow.com/questions/14299149/how-to-use-spring-mvcs-mvcresources-tag-in-a-java-application-context/17013442#17013442
 
