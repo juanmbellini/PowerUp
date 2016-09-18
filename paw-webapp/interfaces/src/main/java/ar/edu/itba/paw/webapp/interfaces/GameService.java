edcfa3fb3493239df81c63324c65c8b16e2de8f3
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-18 04:16:25 -0300

    Modify GameService interface to accept a Map of filters

--- paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/webapp/interfaces/GameService.java
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/webapp/interfaces/GameService.java
@@ -1,12 +1,15 @@
 package ar.edu.itba.paw.webapp.interfaces;
 
-import ar.edu.itba.paw.webapp.model.Filter;
+import ar.edu.itba.paw.webapp.model.FilterCategory;
 import ar.edu.itba.paw.webapp.model.Game;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 
 public interface GameService {
 
     //TODO change Object to Game, rebase with master branch
-    Collection<Game> searchGame(String name, Collection<Filter> filters);
+    Collection<Game> searchGame(String name, Map<FilterCategory,List<String>> filters);
 }
--- paw-webapp/service/src/main/java/ar/edu/itba/paw/webapp/service/GameServiceImpl.java
+++ paw-webapp/service/src/main/java/ar/edu/itba/paw/webapp/service/GameServiceImpl.java
@@ -2,12 +2,14 @@ package ar.edu.itba.paw.webapp.service;
 
 import ar.edu.itba.paw.webapp.interfaces.GameDao;
 import ar.edu.itba.paw.webapp.interfaces.GameService;
-import ar.edu.itba.paw.webapp.model.Filter;
+import ar.edu.itba.paw.webapp.model.FilterCategory;
 import ar.edu.itba.paw.webapp.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class GameServiceImpl implements GameService {
@@ -20,7 +22,7 @@ public class GameServiceImpl implements GameService {
     GameDao gameDao;
 
 
-    public Collection<Game> searchGame(String name, Collection<Filter> filters) {
+    public Collection<Game> searchGame(String name, Map<FilterCategory, List<String>> filters) {
         return gameDao.searchGame(name, filters);
     }
 
