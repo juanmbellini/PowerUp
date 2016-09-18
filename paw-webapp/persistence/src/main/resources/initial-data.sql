f7fa2afc4ab42be6db31b9449c6cb6b0da0e61f0
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-18 19:09:23 -0300

    Added initial data resource

---
+++ paw-webapp/persistence/src/main/resources/initial-data.sql
@@ -0,0 +1,25 @@
+
+-- Genres insert
+BEGIN;
+INSERT INTO power_up.genres ("id", "name") VALUES (33, 'Arcade');
+INSERT INTO power_up.genres ("id", "name") VALUES (32, 'Indie');
+INSERT INTO power_up.genres ("id", "name") VALUES (31, 'Adventure');
+INSERT INTO power_up.genres ("id", "name") VALUES (30, 'Pinball');
+INSERT INTO power_up.genres ("id", "name") VALUES (26, 'Quiz/Trivia');
+INSERT INTO power_up.genres ("id", "name") VALUES (25, 'Hack and slash/Beat ''em up');
+INSERT INTO power_up.genres ("id", "name") VALUES (24, 'Tactical');
+INSERT INTO power_up.genres ("id", "name") VALUES (16, 'Turn-based strategy (TBS)');
+INSERT INTO power_up.genres ("id", "name") VALUES (15, 'Strategy');
+INSERT INTO power_up.genres ("id", "name") VALUES (14, 'Sport');
+INSERT INTO power_up.genres ("id", "name") VALUES (13, 'Simulator');
+INSERT INTO power_up.genres ("id", "name") VALUES (12, 'Role-playing (RPG)');
+INSERT INTO power_up.genres ("id", "name") VALUES (11, 'Real Time Strategy (RTS)');
+INSERT INTO power_up.genres ("id", "name") VALUES (10, 'Racing');
+INSERT INTO power_up.genres ("id", "name") VALUES (9, 'Puzzle');
+INSERT INTO power_up.genres ("id", "name") VALUES (8, 'Platform');
+INSERT INTO power_up.genres ("id", "name") VALUES (7, 'Music');
+INSERT INTO power_up.genres ("id", "name") VALUES (5, 'Shooter');
+INSERT INTO power_up.genres ("id", "name") VALUES (4, 'Fighting');
+INSERT INTO power_up.genres ("id", "name") VALUES (2, 'Point-and-click');
+COMMIT;
+
