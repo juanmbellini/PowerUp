569927bf4c1992c45e05200b7f650b94671b17ce
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-18 04:21:28 -0300

    Add filters param support
    - Filters can be specified as a json in the query string using the following format:
    	/search?name=GameName&filters={"filter1":["value1", "value2"], "filter2":["value1"]}
    	* Note that each filter must be specified in its singular form (i.e. genre instead of genres).
    - Filters json is transformed into a Map<FilterCategory, List<String>>

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MainController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/MainController.java
@@ -1,18 +1,21 @@
 package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.webapp.interfaces.GameService;
-import ar.edu.itba.paw.webapp.model.Filter;
+import ar.edu.itba.paw.webapp.model.FilterCategory;
 import ar.edu.itba.paw.webapp.model.Game;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
 
 @Controller
 public class MainController {
@@ -32,34 +35,45 @@ public class MainController {
         return mav;
     }
 
-    @RequestMapping("/results")
-    public ModelAndView results() {
-        final ModelAndView mav = new ModelAndView("results");
-        mav.addObject("greeting", "PAW");
-        return mav;
-    }
+//    @RequestMapping("/results")
+//    public ModelAndView results() {
+//        final ModelAndView mav = new ModelAndView("results");
+//        mav.addObject("greeting", "PAW");
+//        return mav;
+//    }
+//
+//    @RequestMapping("/search")
+//    public ModelAndView search() {
+//        final ModelAndView mav = new ModelAndView("search");
+//        mav.addObject("greeting", "PAW");
+//        return mav;
+//    }
+
+
 
     @RequestMapping("/search")
-    public ModelAndView search() {
-        final ModelAndView mav = new ModelAndView("search");
-        mav.addObject("greeting", "PAW");
-        return mav;
-    }
-
-
-
-    @RequestMapping("/gameSearch")
-    public ModelAndView searchGameByName(@RequestParam("name") String name,
-                                         @RequestParam(value="genre", required = false) String filterGenre,
-                                         @RequestParam(value = "publisher", required = false) String filterPublisher
-
+    public ModelAndView search(@RequestParam("name") String name,
+                                         @RequestParam(value = "genre", required = false) String filterGenre,
+                                         @RequestParam(value = "publisher", required = false) String filterPublisher,
+                                         @RequestParam(value = "filters", required = false) String filtersJson
                                         ){
+
+        Map<FilterCategory, List<String>> filters = null;
+        try {
+            filters = new ObjectMapper().readValue(
+                    filtersJson, new TypeReference<HashMap<FilterCategory, ArrayList<String>>>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Collection<?> searchedGame = gameService.searchGame(name, filters);
+
         final ModelAndView mav = new ModelAndView("gameSearch");
-        HashSet<Filter> filters = new HashSet<Filter>();
-        if(filterGenre!=null) filters.add(new Filter(Filter.FilterCategory.GENRES,filterGenre));
-        if(filterPublisher!=null) filters.add(new Filter(Filter.FilterCategory.PUBLISHERS,filterPublisher));
-       Collection<Game> searchedGame = gameService.searchGame(name, filters);
-       mav.addObject("gameList", searchedGame);
+        Game game = null;
+//        HashSet<Filter> filters = new HashSet<Filter>();
+//        if (filterGenre != null) filters.add(new Filter(Filter.FilterCategory.GENRES,filterGenre));
+//        if(filterPublisher!=null) filters.add(new Filter(Filter.FilterCategory.PUBLISHERS,filterPublisher));
+//        Collection<Game> searchedGame = gameService.searchGame(name, filters);
+//        mav.addObject("gameList", searchedGame);
         return mav;
     }
 
