5909accbe0ffa8ff3ca9c310ceaf80c1fc9b5e10
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-09-18 19:51:41 -0300

    Add install script
    - Run this script once to create user and password for the app

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e # Aborts script if anything goes wrong
+
+psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='paw';" | grep -q 1 || createuser -d paw;
+psql postgres -tAc "ALTER USER paw WITH PASSWORD 'paw';";
+psql -lqt | cut -d\| -f 1 | grep -wq paw || createdb paw
