bc4c9730c647f08d65d554a4938db21de99400a2
Juan Li Puma <juanlipuma94@gmail.com>
2016-10-10 13:16:59 -0300

    Require ID for user creation

--- paw-webapp/model/src/main/java/ar/edu/itba/paw/webapp/model/User.java
+++ paw-webapp/model/src/main/java/ar/edu/itba/paw/webapp/model/User.java
@@ -1,17 +1,19 @@
 package ar.edu.itba.paw.webapp.model;
 
 public class User {
+    private long id;
     private String username;
     private String email;
     //TODO track ranked games in model?
 
-    public User(String email, String username) {
+    public User(long id, String email, String username) {
+        this.id = id;
         this.email = email;
         this.username = username;
     }
 
-    public User(String email) {
-        this(email, null);
+    public User(long id, String email) {
+        this(id, email, null);
     }
 
     public String getUsername() {
@@ -20,5 +22,9 @@ public class User {
 
     public String getEmail() {
         return email;
+    }
+
+    public long getId() {
+        return id;
     }
 }
