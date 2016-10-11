dfc0ff9051bae086d99db856a76e661ed3eec4ad
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-10-11 05:45:22 -0300

    Change Page model
    - Remove contructor
    - Add setters

--- paw-webapp/model/src/main/java/ar/edu/itba/paw/webapp/model/Page.java
+++ paw-webapp/model/src/main/java/ar/edu/itba/paw/webapp/model/Page.java
@@ -3,7 +3,10 @@ package ar.edu.itba.paw.webapp.model;
 import java.util.Collection;
 
 /**
- * Class representing a search result page
+ * Class representing a search result page.
+ * <p>
+ * In order to use this class, {@code totalPages}, {@code pageNumber}, and {@code pageSize}
+ * must be set with values different than {@code 0} before setting data.
  * <p>
  * Created by Juan Marcos Bellini on 11/10/16.
  * Questions at jbellini@itba.edu.ar or juanmbellini@gmail.com
@@ -14,34 +17,20 @@ public class Page<T> {
     /**
      * The total amount of pages there are in the search done to obtain the data included in this page
      */
-    private int totalPages;
+    private int totalPages = 0;
     /**
      * This page number
      */
-    private int pageNumber;
+    private int pageNumber = 0;
     /**
      * The total amount of elements there are in this page
      */
-    private int pageSize;
+    private int pageSize = 0;
     /**
      * The data included in this page
      */
-    private Collection<T> data;
+    private Collection<T> data = null;
 
-
-    /**
-     * Constructor.
-     *
-     * @param pageNumber This page number.
-     * @param pageSize   This page size (i.e. the amount of elements in this page).
-     */
-    public Page(int pageNumber, int pageSize) {
-        if (pageNumber <= 0 || pageSize <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-    }
 
     /**
      * Total pages getter.
@@ -92,11 +81,38 @@ public class Page<T> {
     }
 
     /**
+     * Page number setter.
+     *
+     * @param pageNumber This page's number.
+     */
+    public void setPageNumber(int pageNumber) {
+        if (pageNumber <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.pageNumber = pageNumber;
+    }
+
+    /**
+     * Page size setter.
+     *
+     * @param pageSize This page's size.
+     */
+    public void setPageSize(int pageSize) {
+        if (pageSize <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.pageSize = pageSize;
+    }
+
+    /**
      * Data setter.
      *
      * @param data The data to be added into this page.
      */
     public void setData(Collection<T> data) {
+        if (pageSize == 0 || pageNumber == 0 || totalPages == 0) {
+            throw new IllegalStateException();
+        }
         if (data.size() > pageSize) {
             throw new IllegalArgumentException();
         }
