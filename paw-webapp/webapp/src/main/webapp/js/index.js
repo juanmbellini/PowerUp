ac56e953ce44e8a2d169a3d49d9bc83b0e2d734f
Juan Li Puma <juan.lipuma@restorando.com>
2016-10-17 18:47:40 -0300

    Use c:url for submitting home search form

--- paw-webapp/webapp/src/main/webapp/js/index.js
+++ paw-webapp/webapp/src/main/webapp/js/index.js
@@ -1,13 +1,8 @@
 $(function() {
-    $("#search-form").on("submit", function(event) {
-        event.preventDefault();
-        var searchTerm = $(this).find("input[name='search']").val();
-        if(searchTerm) {
-            window.location = "/search?name=" + encodeURIComponent(searchTerm);
-        }
-    });
-
     $("#search-icon").on('click', function() {
-        $("#search-form").trigger('submit');
+        var $form = $("#search-form");
+        if($form.find("input[name='search']").val().length > 0) {
+            $form.trigger('submit');
+        }
     });
 });
