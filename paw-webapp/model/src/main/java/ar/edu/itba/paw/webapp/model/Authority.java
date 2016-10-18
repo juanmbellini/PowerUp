704f2025962cfaf1112fa6d8b5f615590babe064
Juan Li Puma <juanlipuma94@gmail.com>
2016-10-18 10:22:36 -0300

    Decouple Authority enum from Spring Security's GrantedAuthority interface
    - Map enum to GrantedAuthority in UserDetailsService
    - Remove model dependency on Spring Security

--- paw-webapp/model/src/main/java/ar/edu/itba/paw/webapp/model/Authority.java
+++ paw-webapp/model/src/main/java/ar/edu/itba/paw/webapp/model/Authority.java
@@ -1,15 +1,8 @@
 package ar.edu.itba.paw.webapp.model;
 
-import org.springframework.security.core.GrantedAuthority;
-
 /**
  * Enum representing granted user authorities.
  */
-public enum Authority implements GrantedAuthority {
+public enum Authority {
     USER;
-
-    @Override
-    public String getAuthority() {
-        return name();
-    }
 }
