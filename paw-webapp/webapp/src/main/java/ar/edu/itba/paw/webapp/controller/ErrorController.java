c247b36b9bcf06d5d0ad5a421c83f55aba8b546b
Juan Marcos Bellini <juanmbellini@gmail.com>
2016-10-19 04:06:53 -0300

    Add Multiple controllers support
    - Remove MainController
    - Add abstract BaseController, which implements methods that all controllers will use
    - Add HomeController, which handles requests to home page
    - Add ErrorController, which handles errors (400, 404, and 500).
    - Add SessionController, which handles login request
    - Add UserController, which handles users operations requests
    - Add GameController, which handles games operations requests

---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ErrorController.java
@@ -0,0 +1,43 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.webapp.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Juan Marcos Bellini on 19/10/16.
+ * Questions at jbellini@itba.edu.ar or juanmbellini@gmail.com
+ *
+ * This controller is in charge of handling errors.
+ */
+@Controller
+public class ErrorController extends BaseController {
+
+
+    @Autowired
+    public ErrorController(UserService userService) {
+        super(userService);
+    }
+
+
+    @RequestMapping("/error500")
+    public ModelAndView error500() {
+        final ModelAndView mav = new ModelAndView("error500");
+        return mav;
+    }
+
+    @RequestMapping("/error404")
+    public ModelAndView error404() {
+        final ModelAndView mav = new ModelAndView("error404");
+        return mav;
+    }
+
+    @RequestMapping("/error400")
+    public ModelAndView error400() {
+        final ModelAndView mav = new ModelAndView("error400");
+        return mav;
+    }
+
+}
---
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/UserController.java
@@ -0,0 +1,143 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.webapp.exceptions.UserExistsException;
+import ar.edu.itba.paw.webapp.form.LoginForm;
+import ar.edu.itba.paw.webapp.form.RateAndStatusForm;
+import ar.edu.itba.paw.webapp.form.UserForm;
+import ar.edu.itba.paw.webapp.interfaces.GameService;
+import ar.edu.itba.paw.webapp.interfaces.UserService;
+import ar.edu.itba.paw.webapp.model.Game;
+import ar.edu.itba.paw.webapp.model.PlayStatus;
+import ar.edu.itba.paw.webapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Juan Marcos Bellini on 19/10/16.
+ * Questions at jbellini@itba.edu.ar or juanmbellini@gmail.com
+ * <p>
+ * This controller is in charge of handling users' operations request.
+ */
+@Controller
+public class UserController extends BaseController {
+
+
+    /**
+     * A game service used for listing games.
+     */
+    private GameService gameService;
+    /**
+     * A password encoder used for hashing passwords when creating users.
+     */
+    private PasswordEncoder passwordEncoder; // TODO: move this to user service [JMB]
+
+
+    @Autowired
+    public UserController(UserService us, GameService gameService, PasswordEncoder passwordEncoder) {
+        super(us);
+        this.gameService = gameService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @RequestMapping("/list")
+    public ModelAndView list(@RequestParam(value = "username", required = false) String username) {
+
+        // TODO: Check if we are really allowing anyone to check other's lists. [JMB]
+        if (username == null) {
+            if (!isLoggedIn()) {
+                return new ModelAndView("redirect:error400");
+            }
+            return new ModelAndView("redirect:/list?username=" + getCurrentUsername());
+        }
+        final ModelAndView mav = new ModelAndView("list");
+        User u = getUserService().findByUsername(username);
+        if (u == null) return new ModelAndView("error400");
+
+
+        Map<PlayStatus, Set<Game>> gamesInListsMap = new HashMap<>();
+        for (PlayStatus playStatus : PlayStatus.values()) {
+            // TODO use other set and give it order? ScoreOrder? (If treeSet is used, danger of eliminating games)
+            gamesInListsMap.put(playStatus, new HashSet<>());
+        }
+        Map<Long, PlayStatus> playStatuses = u.getPlayStatuses();
+        // TODO do this in user?
+        for (long gameId : playStatuses.keySet()) {
+            Game game = gameService.findById(gameId);
+            if (game == null) throw new IllegalStateException("Status list should have a game that do not exist");
+            gamesInListsMap.get(playStatuses.get(gameId)).add(game);
+        }
+        mav.addObject("user", u);
+        mav.addObject("playStatuses", gamesInListsMap);
+
+        return mav;
+    }
+
+
+    @RequestMapping("/register")
+    public ModelAndView registerGet(@ModelAttribute("registerForm") final UserForm form) {
+        return new ModelAndView("register");
+    }
+
+    @RequestMapping(value = "/register", method = {RequestMethod.POST})
+    public ModelAndView registerPost(@Valid @ModelAttribute("registerForm") final UserForm form,
+                                     final BindingResult errors,
+                                     final RedirectAttributes redirectAttributes) {
+
+        if (errors.hasErrors()) {
+            redirectAttributes.addFlashAttribute("registerForm", form);
+            return new ModelAndView("redirect:/register");
+        }
+        final String email = form.getEmail(),
+                // TODO: Move encryption to UserService [JMB]
+                hashedPassword = passwordEncoder.encode(form.getPassword()),
+                username = form.getUsername();
+
+        User user;
+        try {
+            user = getUserService().create(email, hashedPassword, username);
+        } catch (UserExistsException e) {
+            String msgLow = e.getMessage().toLowerCase();
+            // The calls to rejectValue will add errors to the UserForm so they get displayed properly and in the proper fields
+            if (msgLow.contains("email")) {
+                errors.rejectValue("email", "error.email", "An account exists with this email");
+            } else if (msgLow.contains("username")) {
+                errors.rejectValue("username", "error.username", "Username already taken");
+            } else {
+                errors.addError(new ObjectError("error", "Error creating account, please try again"));
+                System.err.println("Unrecognized message in UserExistsException: \"" + e.getMessage() + "\".");
+                System.err.println("Printing stack trace:");
+                e.printStackTrace();
+            }
+            redirectAttributes.addFlashAttribute("registerForm", form);
+            return new ModelAndView("redirect:/register");
+        }
+        // TODO: Remove this println [JMB]
+        System.out.println("Registered user " + user.getUsername() + " with email " + user.getEmail() + ", logging them in and redirecting to home");
+
+        //Log the new user in
+        Authentication auth = new UsernamePasswordAuthenticationToken(username, hashedPassword);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return new ModelAndView("redirect:/");
+    }
+
+
+}
