10f4810b9e3eb06fac957401d9954d8057ff061d
Juan Li Puma <juan.lipuma@restorando.com>
2016-10-26 21:03:56 -0300

    Implement removable filters
    - Alter controller to send extra information about applied filters to Search page
    - Add collapsible with applicable filters, divided in categories with tabs
    - Add Materialize clickable chips to toggle different filters
    - Restructure Search page elements
    - Add JS logic to handle chip behavior
    
    TODO
    - Remove Advanced Search page, all search functionality will be served through the Search page

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GameController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GameController.java
@@ -116,7 +116,7 @@ public class GameController extends BaseController {
             return mav;
         }
 
-        Map<FilterCategory, List<String>> filters;
+        Map<FilterCategory, List<String>> filters = null;
         try {
             filters = objectMapper.readValue(filtersStr, typeReference);
 
@@ -144,6 +144,20 @@ public class GameController extends BaseController {
         } catch (IOException | NumberFormatException | IllegalPageException e) {
             e.printStackTrace();  // Wrong filtersJson, pageSizeStr or pageNumberStr, or pageNumber strings
             mav.setViewName("redirect:/error400");
+        }
+
+        //Add all possible filters (categories and values) for the filters section,
+        //indicating whether the filter is applied or not
+        for (FilterCategory filterCategory : FilterCategory.values()) {
+            try {
+                Set<Object[]> values = new LinkedHashSet<>();
+                for(String value : gameService.getFiltersByType(filterCategory)) {
+                    values.add(new Object[] {value, filters != null && filters.containsKey(filterCategory) && filters.get(filterCategory).contains(value)});
+                }
+                mav.addObject(English.plural(filterCategory.name()).toUpperCase(), values);
+            } catch (Exception e) {
+                return new ModelAndView("redirect:/error500");
+            }
         }
         return mav;
     }
@@ -244,6 +258,4 @@ public class GameController extends BaseController {
         }
         return mav;
     }
-
-
 }
