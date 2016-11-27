4e74ea8931bebcc0635c4beb6498788119bbd25c
Juan Li Puma <juanlipuma94@gmail.com>
2016-11-27 14:04:50 -0300

    Remove static entity manager from helper, implement more methods

--- paw-webapp/persistence/src/main/java/ar/edu/itba/paw/webapp/persistence/DaoHelper.java
+++ paw-webapp/persistence/src/main/java/ar/edu/itba/paw/webapp/persistence/DaoHelper.java
@@ -4,7 +4,7 @@ import ar.edu.itba.paw.webapp.exceptions.NoSuchEntityException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.TypedQuery;
 import java.util.Collections;
 import java.util.List;
@@ -12,22 +12,31 @@ import java.util.List;
 /**
  * DAO helper class. Provides package-private static methods useful to all DAOs.
  */
-public class DaoHelper {
-
-    @PersistenceContext
-    //TODO this may not work; if it doesn't, implement singleton
-    private static EntityManager entityManager;
+/*package*/ class DaoHelper {
 
     /**
-     * Attempts to find an entity. If found, returns it, otherwise throws exception.
+     * Attempts to find an entity by ID. If found, returns it, otherwise returns {@code null}.
      *
-     * @param klass The class of the entity to find
-     * @param id The ID of the entity to find.
-     * @return The found entity.
+     * @param entityManager An entity manager with which to perform queries.
+     * @param klass         The class of the entity to find.
+     * @param id            The ID of the entity to find.
+     * @return              The found entity, or {@code null} if not found.
+     */
+    /*package*/ static <T> T findSingle(EntityManager entityManager, Class<T> klass, long id) {
+        return entityManager.find(klass, id);
+    }
+
+    /**
+     * Attempts to find an entity by ID. If found, returns it, otherwise throws exception.
+     *
+     * @param entityManager An entity manager with which to perform queries.
+     * @param klass         The class of the entity to find.
+     * @param id            The ID of the entity to find.
+     * @return              The found entity.
      * @throws NoSuchEntityException If no such entity exists.
      */
-    /*package*/ static <T> T find(Class<T> klass, long id) {
-        T result = entityManager.find(klass, id);
+    /*package*/ static <T> T findSingleOrThrow(EntityManager entityManager, Class<T> klass, long id) throws NoSuchEntityException {
+        T result = findSingle(entityManager, klass, id);
         if (result == null) {
             throw new NoSuchEntityException(klass, id);
         }
@@ -37,42 +46,62 @@ public class DaoHelper {
     /**
      * Performs a basic search with parameters, returning a single result.
      *
-     * @param klass The class of the object type to return.
-     * @param baseQuery The base HQL query.
-     * @param parameters Parameters for the query, in order of appearance in the query.
-     * @param <T> The return type.
+     * @param <T>           The return type.
+     * @param entityManager An entity manager with which to perform queries.
+     * @param klass         The class of the object type to return.
+     * @param baseQuery     The base HQL query. <b>NOTE:</b> Parameters must be numbered instead of named,
+     *                      e.g. {@code "FROM User AS U WHERE U.username = ?1}"
+     * @param parameters    Parameters for the query, in order of appearance in the query.
      * @return The found entity, or {@code null} if not found.
      * @throws javax.persistence.NonUniqueResultException If more than one result is found.
      */
-    /*package*/ static <T> T findSingleWithConditions(Class<T> klass, String baseQuery, Object ... parameters) {
+    /*package*/ static <T> T findSingleWithConditions(EntityManager entityManager, Class<T> klass, String baseQuery, Object... parameters) throws NonUniqueResultException {
         TypedQuery<T> query = entityManager.createQuery(baseQuery, klass);
         for (int i = 0; i < parameters.length; i++) {
-            query.setParameter(i, parameters[i]);
+            query.setParameter(i+1, parameters[i]);
         }
         try {
             return query.getSingleResult();
-        } catch(NoResultException e) {
+        } catch (NoResultException e) {
             return null;
+        }
+    }
+
+    /**
+     * Attempts to find a all entities of a specified class without specifying a sorting criterion.
+     *
+     * @param entityManager An entity manager with which to perform queries.
+     * @param klass         The class of the entities to find.
+     * @return              The found entities.
+     */
+    /*package*/ static <T> List<T> findAll(EntityManager entityManager, Class<T> klass) {
+        TypedQuery<T> query = entityManager.createQuery("FROM " + klass.getName(), klass);
+        try {
+            return query.getResultList();
+        } catch (NoResultException e) {
+            return Collections.emptyList();
         }
     }
 
     /**
      * Performs a basic search with parameters, returning a list of results.
      *
-     * @param klass The class of the object type to return.
-     * @param baseQuery The base HQL query.
-     * @param parameters Parameters for the query, in order of appearance in the query.
-     * @param <T> The return type.
-     * @return A list with the matching entities, or an empty list of nothing is found if not found.
+     * @param <T>           The return type.
+     * @param entityManager An entity manager with which to perform queries.
+     * @param klass         The class of the object type to return.
+     * @param baseQuery     The base HQL query. <b>NOTE:</b> Parameters must be numbered instead of named,
+     *                      e.g. {@code "FROM User AS U WHERE U.username LIKE ?1}"
+     * @param parameters    Parameters for the query, in order of appearance in the query.
+     * @return              A list with the matching entities, or an empty list of nothing is found if not found.
      */
-    public static <T> List<T> findManyWithConditions(Class<T> klass, String baseQuery, Object[] parameters) {
+    /*package*/ static <T> List<T> findManyWithConditions(EntityManager entityManager, Class<T> klass, String baseQuery, Object... parameters) {
         TypedQuery<T> query = entityManager.createQuery(baseQuery, klass);
         for (int i = 0; i < parameters.length; i++) {
-            query.setParameter(i, parameters[i]);
+            query.setParameter(i+1, parameters[i]);
         }
         try {
             return query.getResultList();
-        } catch(NoResultException e) {
+        } catch (NoResultException e) {
             return Collections.emptyList();
         }
     }
