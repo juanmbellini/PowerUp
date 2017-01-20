b1f4416d10bb991680bfc79e36e1feca29464828
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-01-20 04:04:39 -0300

    Remove UpdatableEntityNotPresentException
    - Now NoSuchEntityException is used.

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/exception_mappers/NoSuchEntityExceptionMapper.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/exception_mappers/NoSuchEntityExceptionMapper.java
@@ -1,7 +1,7 @@
 package ar.edu.itba.paw.webapp.exception_mappers;
 
 
-import ar.edu.itba.paw.webapp.exceptions.UpdatableEntityNotPresentException;
+import ar.edu.itba.paw.webapp.exceptions.NoSuchEntityException;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -11,9 +11,9 @@ import javax.ws.rs.ext.Provider;
  * Created by Juan Marcos Bellini on 22/12/16.
  */
 @Provider
-public class UpdatableEntityNotPresentExceptionMapper implements ExceptionMapper<UpdatableEntityNotPresentException> {
+public class NoSuchEntityExceptionMapper implements ExceptionMapper<NoSuchEntityException> {
     @Override
-    public Response toResponse(UpdatableEntityNotPresentException exception) {
+    public Response toResponse(NoSuchEntityException exception) {
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
