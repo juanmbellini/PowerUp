66285375a15a901cc4ecce8b6073e0d311a59d6d
Juan Marcos Bellini <juanmbellini@gmail.com>
2017-01-20 05:12:56 -0300

    Add filters and name support to games collection resource
    - Now search can be done by name, and applying publishers, developers, genres, platforms and keywords filters
    - Also, default values were given ("" for name, empty lists for filters, "name" for orderCategory, "25" for pageSize, "true" for ascending and "1" for pageNumber)
    
    - Note: for searching applying filters, use query params as follows: /games&genre=RPG&genre=Adventure&publisher=Nintendo&publisher=Square
        - This will generate a List<String> containing ["RPG", "Adventure"] (for genres), and ["Nintendo", "Square"] (for publishers)

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GameJerseyController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/GameJerseyController.java
@@ -2,6 +2,7 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.webapp.dto.GameDto;
 import ar.edu.itba.paw.webapp.interfaces.GameService;
+import ar.edu.itba.paw.webapp.model.FilterCategory;
 import ar.edu.itba.paw.webapp.model.Game;
 import ar.edu.itba.paw.webapp.model.OrderCategory;
 import ar.edu.itba.paw.webapp.utilities.Page;
@@ -11,6 +12,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Juan Marcos Bellini on 8/1/17.
@@ -30,13 +32,24 @@ public class GameJerseyController {
         this.gameService = gameService;
     }
 
+
     @GET
     @Path("/")
-    public Response getAllGames(@QueryParam("orderCategory") final OrderCategory orderCategory,
-                                @QueryParam("ascending") final boolean ascending,
-                                @QueryParam("pageSize") final int pageSize,
-                                @QueryParam("pageNumber") final int pageNumber) {
-        Page<Game> games = gameService.searchGames("", new HashMap<>(), orderCategory, ascending, pageSize, pageNumber);
+    public Response getAllGames(@QueryParam("orderCategory") @DefaultValue("name") final OrderCategory orderCategory,
+                                @QueryParam("ascending") @DefaultValue("true") final boolean ascending,
+                                @QueryParam("pageSize") @DefaultValue("25") final int pageSize,
+                                @QueryParam("pageNumber") @DefaultValue("1") final int pageNumber,
+                                // Filters
+                                @QueryParam("name") @DefaultValue("") final String name,
+                                @QueryParam("publisher") final List<String> publishers,
+                                @QueryParam("developer") final List<String> developers,
+                                @QueryParam("genre") final List<String> genres,
+                                @QueryParam("keyword") final List<String> keywords,
+                                @QueryParam("platform") final List<String> platforms) {
+
+        Page<Game> games = gameService.searchGames(name,
+                createFiltersMap(publishers, developers, genres, keywords, platforms),
+                orderCategory, ascending, pageSize, pageNumber);
         return Response
                 .ok(new GenericEntity<List<GameDto>>(GameDto.createList(games.getData())) {
                 })
@@ -60,5 +73,28 @@ public class GameJerseyController {
         final Game game = gameService.findById(id);
         return game == null ?
                 Response.status(Response.Status.NOT_FOUND).build() : Response.ok(new GameDto(game)).build();
+    }
+
+    
+    /**
+     * Create a filers map.
+     *
+     * @param publishers The list with the publishers values.
+     * @param developers The list with the developers values.
+     * @param genres     The list with the genres values.
+     * @param keywords   The list with the keywords values.
+     * @param platforms  The list with the platforms values.
+     * @return The filters map.
+     */
+    private static Map<FilterCategory, List<String>> createFiltersMap(List<String> publishers, List<String> developers,
+                                                                      List<String> genres, List<String> keywords,
+                                                                      List<String> platforms) {
+        final Map<FilterCategory, List<String>> filters = new HashMap<>();
+        filters.put(FilterCategory.publisher, publishers);
+        filters.put(FilterCategory.developer, developers);
+        filters.put(FilterCategory.genre, genres);
+        filters.put(FilterCategory.keyword, keywords);
+        filters.put(FilterCategory.platform, platforms);
+        return filters;
     }
 }
