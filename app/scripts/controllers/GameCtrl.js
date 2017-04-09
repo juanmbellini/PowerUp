3db0f8cca3631565aac26fe6d38218f251cc4236
Juan Li Puma <juanlipuma94@gmail.com>
2017-04-09 15:36:44 -0300

    Improve code, fix loading circle directive
    - Find game in Game page only once with ngInit
    - Fire event when game is found, to retrieve reviews
    - Add code to hit API endpoint, comment it out because endpoint is not fully implemented yet
    - Add "Write Review" button and logic
    - Fix loading circle directive

--- app/scripts/controllers/GameCtrl.js
+++ app/scripts/controllers/GameCtrl.js
@@ -1,32 +1,39 @@
 'use strict';
-define(['powerUp', 'slick-carousel', 'onComplete', 'loadingCircle'], function(powerUp) {
+define(['powerUp', 'slick-carousel', 'onComplete', 'loadingCircle', 'sessionService'], function(powerUp) {
 
-    powerUp.controller('GameCtrl', ['$scope', '$location', 'Restangular', function($scope, $location, Restangular) {
+    powerUp.controller('GameCtrl', ['$scope', '$location', 'Restangular', 'SessionService', function($scope, $location, Restangular, SessionService) {
 
         Restangular.setFullResponse(false);
         $scope.gameId = $location.search().id;
-        $scope.reviews = null;
+        $scope.game = null;
 
-        Restangular.one('games',$scope.gameId).get().then(function(game) {
-            $scope.game = game;
+        $scope.findGame = function(gameId) {
+            Restangular.one('games', gameId).get().then(function(game) {
+                $scope.game = game;
+                $scope.gameId = game.id;
 
-            console.log('Game: ', game);
-            if ($scope.gameId > 0 && $scope.game !== null) {
-                // TODO fire review search
-                $scope.videosMin = Math.min($scope.game.videoUrls.length, 4);       // How many videos to show per carousel page
-                $scope.picturesMin = Math.min($scope.game.pictureUrls.length, 4);   // How many pictures to show per carousel page
-            } else {
-                // TODO show 'game not found'
+                console.log('Game: ', game);
+                if ($scope.gameId > 0 && $scope.game !== null) {
+                    $scope.videosMin = Math.min($scope.game.videoUrls.length, 4);       // How many videos to show per carousel page
+                    $scope.picturesMin = Math.min($scope.game.pictureUrls.length, 4);   // How many pictures to show per carousel page
+                    $scope.$broadcast('gameFound');                                     // Game found, fire all secondary searches (e.g. reviews)
+                } else {
+                    // TODO show 'game not found'
+                    $location.search({});
+                    $location.path('');
+                }
+            }, function(response) {
+                console.log('Error with status code', response.status); // TODO handle error
                 $location.search({});
                 $location.path('');
-            }
-        }, function(response) {
-            console.log('Error with status code', response.status); // TODO handle error
-            $location.search({});
-            $location.path('');
-        });
+            });
 
-        // Event receivers for ng-repeats, see http://stackoverflow.com/questions/15207788/calling-a-function-when-ng-repeat-has-finished
+        };
+
+        /* *******************************
+         * Event receivers for ng-repeats, see http://stackoverflow.com/questions/15207788/calling-a-function-when-ng-repeat-has-finished
+         * ******************************/
+        // (Re-)initialize Slick for game pictures
         $scope.$on('picturesRendered', function(event) {
             angular.element('#screenshots-carousel').slick({
                 infinite: false,
@@ -35,7 +42,7 @@ define(['powerUp', 'slick-carousel', 'onComplete', 'loadingCircle'], function(po
             });
             require(['lightbox2']);
         });
-
+        // (Re-)initialize Slick for game videos
         $scope.$on('videosRendered', function(event) {
             angular.element('#videos-carousel').slick({
                 infinite: false,
@@ -48,6 +55,30 @@ define(['powerUp', 'slick-carousel', 'onComplete', 'loadingCircle'], function(po
         /* *****************************************
          *                 REVIEWS
          * ****************************************/
+        $scope.reviews = null;
 
+        $scope.$on('gameFound', function() {
+            // Restangular.all('reviews').getList({gameId: $scope.game.id}).then(function (reviews) {
+            //     $scope.reviews = reviews;
+            // }, function() {
+            //     console.log('There was an error getting reviews');
+            // });
+        });
+
+        $scope.canWriteReview = function() {
+            if ($scope.reviews === null || !SessionService.isLoggedIn()) {
+                return false;
+            } else {
+                // Can submit review if user hasn't made one already. For now we just check that the retrieved reviews don't have the current user as author.
+                // TODO consider hitting API for this since reviews may be paginated
+                var currentUserId = SessionService.getCurrentUser().id;
+                $scope.reviews.forEach(function(review) {
+                    if (currentUserId === review.user.id) {
+                        return false;
+                    }
+                });
+                return true;
+            }
+        };
     }]);
 });
