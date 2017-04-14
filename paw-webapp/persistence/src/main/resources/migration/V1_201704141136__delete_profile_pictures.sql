a4df9246f3205dc1109b0be899f5e3a607b28b1e
Juan Li Puma <juanlipuma94@gmail.com>
2017-04-14 14:58:59 +0000

    Merged in bug/profile-picture-mime-types (pull request #164)
    Restore profile picture MIME types
    
    * Restore profile picture MIME types
    
        - Initial data migrations left out the MIME types of profile pictures, so they can't be served correctly. This data migration restores said MIME types.
    
    * Add trailing \n
    
    Approved-by: Juan Li Puma <juanlipuma94@gmail.com>

---
+++ paw-webapp/persistence/src/main/resources/migration/V1_201704141136__delete_profile_pictures.sql
@@ -0,0 +1,4 @@
+UPDATE users SET mime_type = 'image/jpeg' WHERE id = 1;
+UPDATE users SET mime_type = 'image/jpeg' WHERE id = 7;
+UPDATE users SET mime_type = 'image/png' WHERE id = 17;
+UPDATE users SET mime_type = 'image/jpeg' WHERE id = 20;
