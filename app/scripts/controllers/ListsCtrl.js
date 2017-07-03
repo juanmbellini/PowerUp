9927c0aa1fdad028d59d24fd5c9469ca5be96aab
Droche <ddegrimaudetderocheb@itba.edu.ar>
2017-07-03 18:51:00 -0300

    Adds api calls to front end in gameList page.

--- app/scripts/controllers/ListsCtrl.js
+++ app/scripts/controllers/ListsCtrl.js
@@ -1,29 +1,52 @@
 'use strict';
 define(['powerUp'], function(powerUp) {
 
-    powerUp.controller('ListsCtrl', function($scope, $location, Restangular, SweetAlert) {
+    powerUp.controller('ListsCtrl', function($scope, $location, Restangular, SweetAlert, $log) {
 
         // TODO replace this values with api calls. -- Droche 15/02/2017
         $scope.playStatuses = ['planToPlay','playing','played'];
-        $scope.shelves = [{name: 'RPG',id: 1},{name: 'Racing',id: 2}];
-        $scope.games = [];
+        $scope.shelves = null;         // [{name: 'RPG',id: 1},{name: 'Racing',id: 2}];
         $scope.userId = $location.search().userId;
-        // TODO change to username or add both options ---droche 13/02/2017
-        Restangular.one('users',$scope.userId).get().then(function(user) {
+        $scope.username = $location.search().username;
+
+        var userURL = null;
+        if ($scope.userId) {
+            userURL = Restangular.one('users',$scope.userId);
+        } else if ($scope.username) {
+            // userURL =  Restangular.all('users').one(username,$scope.username);
+            // TODO username. Paja porque todo lo otro depende de la userURL que no podria armar. tendria que hacer que todo espere
+        } else {
+            $log.debug('Need id in url');
+            // TODO search for logged user (?
+            $location.search({});
+            $location.path('');
+        }
+
+
+        userURL.get().then(function(user) {
             $scope.user = user;
         }, function(response) {
-            console.log('Error with status code', response.status); // TODO handle error
+            $log.error('Could not get user or no user with that id. Error with status code', response.status); // TODO handle error
+            $location.search({});
+            $location.path('');
         });
+
+        userURL.all('shelves').getList({}).then(function(shelves) {
+            $scope.shelves = shelves;
+        }, function(error) {
+            $log.error('Could not get user shelves. Error with status code', error.status);
+        });
+
+        $scope.games = [];
 
 
         // TODO change games to real game list and recommended games too.
         var baseGames = Restangular.all('games');
-        baseGames.getList({})
-        .then(function(games) {
+        baseGames.getList({}).then(function(games) {
             $scope.games = games;
             $scope.recommendedGames = games;
         }, function(response) {
-            console.log('Error with status code', response.status);
+            $log.error('Error with status code', response.status);
         });
 
 
@@ -41,7 +64,14 @@ define(['powerUp'], function(powerUp) {
                 function () {
                     // //Disable submit button to prevent multiple submissions
                     // $('.confirm').attr('disabled', 'disabled');
-                    console.log('Now your shelf should be deleted');
+
+                    userURL.one('shelves',shelf.name).remove().then(function(response) {
+                        $log.debug('Now your shelf should be deleted');
+                    },function(error) {
+
+                        // $log.error('Couldn\'t delete shelf ', error);
+                    });
+
               });
         };
         $scope.editShelf = function(shelf) {
@@ -86,13 +116,34 @@ define(['powerUp'], function(powerUp) {
                     //         return entityMap[s];
                     //     });
                     // }
-                    console.log('Now your shelf should be called ' + inputValue);
+
+                    var oldName = shelf.name;
+                    shelf.name = inputValue;
+                    userURL.one('shelves',oldName).customPUT(shelf).then(function (response) {
+                        $log.debug('Updated Shelf. Now your shelf should be called ' + inputValue);
+                    }, function(response) {
+                        $log.error('Error creating shelf. Error with status code', response.status);
+                        // TODO handle error. show no user found or something
+                    });
                 });
         };
         $scope.isCurrentUserLogged = function() {
-            return true; // TODO
+            return true; // TODO ver auth
         };
-        $scope.deleteGame = function(game) {
+
+        $scope.newShelfName = null;
+        $scope.createShelf = function() {
+            // TODO validate input?
+            var shelf = {name: $scope.newShelfName};
+            userURL.all('shelves').post(shelf).then(function(response) {
+                $log.debug('Created Shelf');
+            }, function(response) {
+                $log.error('Error creating shelf. Error with status code', response.status); // TODO handle error
+            });
+        };
+
+
+        $scope.deleteGame = function(game) { // TODO re pensar si va a existir exto en el nuevo contexto
             SweetAlert.swal({
                     title: 'Are you sure?',
                     text: 'You are about to delete ' + game.name + ' from all your shelves.',
@@ -103,7 +154,7 @@ define(['powerUp'], function(powerUp) {
                     closeOnConfirm: false
                 },
                 function () {
-                    console.log('Now your game should be deleted');
+                    $log.debug('Now your game should be deleted');
                 });
         };
 
