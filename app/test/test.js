c1605d8a4b9a6f5a4c5ecb76e0273defd02f8b2e
Juan Li Puma <juanlipuma94@gmail.com>
2017-07-15 10:49:46 -0300

    Initial working Karma config (thanks José)

--- app/test/test.js
+++ app/test/test.js
@@ -3,22 +3,3 @@ describe("A suite", function() {
         expect(true).toBe(true);
     });
 });
-
-
-describe("A service", function() {
-    var service;
-
-    beforeEach(module('powerUp'));
-    beforeEach(inject(function (PaginationService) {
-        service = PaginationService;
-    }));
-
-    describe('Constructor', function () {
-
-        it('assigns a name', function () {
-            debugger;
-            expect(service).to.have.property('get');
-        });
-
-    });
-});
--- karma.conf.js
+++ karma.conf.js
@@ -12,14 +12,48 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine', 'requirejs'],
 
+    plugins: [
+        require("karma-jasmine"),
+        require("karma-requirejs"),
+        require("karma-ng-html2js-preprocessor"),
+        require("karma-jasmine-html-reporter"),
+        require("karma-chrome-launcher"),
+        require("karma-phantomjs-launcher")
+    ],
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/angular/angular.js',
-      'node_modules/angular-mocks/angular-mocks.js',
-      'test-main.js',
-      {pattern: 'app/test/**.js', included: true},
-      {pattern: 'app/test/**.js', included: true}
+      // Angular
+      {pattern: 'bower_components/angular/angular.js', included: false},
+      {pattern: 'bower_components/angular-local-storage/dist/angular-local-storage.js', included: false},
+      {pattern: 'bower_components/angular-mocks/angular-mocks.js', included: false},
+      {pattern: 'bower_components/angular-route/angular-route.js', included: false},
+      {pattern: 'bower_components/ng-file-upload/ng-file-upload.js', included: false},
+      // {pattern: 'bower_components/ngSweetAlert/SweetAlert.js', included: false},
+      //
+      // Etc.
+      {pattern: 'bower_components/es5-shim/es5-shim.js', included: false},
+      {pattern: 'bower_components/requirejs/require.js', included: false},
+      {pattern: 'bower_components/restangular/dist/restangular.js', included: false},
+
+      // UI
+      {pattern: 'bower_components/jquery/dist/jquery.js', included: false},
+      // TODO use custom jQuery-UI dist in deploy branch
+      {pattern: 'bower_components/jquery-ui/jquery-ui.js', included: false},
+      {pattern: 'bower_components/lightbox2/dist/js/lightbox.js', included: false},
+      {pattern: 'bower_components/materialize/js/*.js', included: false},
+      // See if Hammer is needed, included in Materialize
+      // {pattern: 'bower_components/hammerjs/hammer.js', included: false},
+      // {pattern: 'bower_components/velocity/velocity.js', included: false},
+      {pattern: 'bower_components/moment/moment.js', included: false},
+      {pattern: 'bower_components/slick-carousel/slick/slick.js', included: false},
+      {pattern: 'bower_components/sweetalert/dist/sweetalert.min.js', included: false},
+
+      // Controllers, views, tests, etc.
+      {pattern: 'app/scripts/**/*.js', included: false},
+      {pattern: 'app/views/**/*.html', included: false},
+      {pattern: 'test-main.js', included: true},
+      {pattern: 'app/test/**.js', included: false}
     ],
 
 
@@ -31,13 +65,20 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      // TODO uncomment and see what it does
+      // 'app/scripts/controllers/**/*.js': ['coverage'],
+      // 'app/scripts/directives/*.js': ['coverage'],
+      // 'app/scripts/services/!(dependencyResolverFor).js': ['coverage'],
+      // 'app/views/**/*.html': ['ng-html2js']
     },
+
+    // TODO maybe coverage reporter?
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'kjhtml'/*, 'coverage'*/],
 
 
     // web server port
@@ -59,7 +100,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+    browsers: [/*'Chrome', */'PhantomJS'],
 
 
     // Continuous Integration mode
