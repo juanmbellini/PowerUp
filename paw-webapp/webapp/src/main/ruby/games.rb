2f70a861e3ce1caa8b532f1bd8e3101d9d4a0c2f
Juan Li Puma <juanlipuma94@gmail.com>
2017-11-17 14:02:56 -0300

    Download games

---
+++ paw-webapp/webapp/src/main/ruby/games.rb
@@ -0,0 +1,17 @@
+require 'json'
+require_relative 'query'
+
+game_ids = JSON.parse(File.read('game_ids.json')).map { |entry| entry['id'] }
+
+big_result = []
+i = 0
+game_ids.each_slice(500) do |slice|
+  q = Query.new("#{Query::API_URL}/games/#{slice.join(',')}", 'user-key' => Query::KEY, 'Accept' => 'application/json')
+  puts "Parsing games ##{i}-#{i += 500}"
+  big_result += JSON.parse(q.fire.read)
+end
+
+puts "Done, writing result to file"
+File.open('games', 'w') do |f|
+  f.write(big_result.to_json)
+end
