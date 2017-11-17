f54fd403c8b01f7ed46bce86e138ba12b13550da
Juan Li Puma <juanlipuma94@gmail.com>
2017-11-17 13:41:02 -0300

    Add ruby script to download ALL game IDs

---
+++ paw-webapp/webapp/src/main/ruby/game_ids.rb
@@ -0,0 +1,16 @@
+require 'json'
+require_relative 'query'
+
+q = Query.new("#{Query::API_URL}/games/?limit=50&scroll=0", 'user-key' => Query::KEY, 'Accept' => 'application/json')
+
+big_result = []
+q.get_all do |page|
+  parsed_page = JSON.parse(page)
+  puts parsed_page.to_json
+  big_result += parsed_page
+end
+
+puts "Done, writing result to file"
+File.open('game_ids.json', 'w') do |f|
+  f.write(big_result.to_json)
+end
