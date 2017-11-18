b0d1652413b7d218786d3d80744b60e52f6377a4
Juan Li Puma <juanlipuma94@gmail.com>
2017-11-17 23:18:11 -0300

    Don't select unnecessary fields

--- paw-webapp/persistence/src/main/resources/migration/V1_201711171904__delete_duplicates.sql
+++ paw-webapp/persistence/src/main/resources/migration/V1_201711171904__delete_duplicates.sql
@@ -8,7 +8,7 @@
 
 -- Game keywords
 DELETE FROM game_keywords AS g1 WHERE
-  EXISTS(SELECT * from game_keywords AS g2 WHERE g1.id < g2.id AND g1.game_id = g2.game_id AND g1.keyword_id = g2.keyword_id);
+  EXISTS(SELECT id from game_keywords AS g2 WHERE g1.id < g2.id AND g1.game_id = g2.game_id AND g1.keyword_id = g2.keyword_id);
 CREATE UNIQUE INDEX game_id_keyword_id ON game_keywords (game_id, keyword_id);
 
 -- Game pictures
@@ -16,5 +16,5 @@ CREATE UNIQUE INDEX cloudinary_id ON game_pictures (cloudinary_id);
 
 -- Game videos
 DELETE FROM game_videos AS g1 WHERE
-  EXISTS(SELECT * from game_videos AS g2 WHERE g1.id < g2.id AND g1.game_id = g2.game_id AND g1.video_id = g2.video_id);
+  EXISTS(SELECT id from game_videos AS g2 WHERE g1.id < g2.id AND g1.game_id = g2.game_id AND g1.video_id = g2.video_id);
 CREATE UNIQUE INDEX game_id_video_id ON game_videos (game_id, video_id);
