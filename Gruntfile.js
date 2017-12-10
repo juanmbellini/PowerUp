9d4ef5c757ca761f26818b9da43ceb4697b8ef80
Juan Li Puma <juanlipuma94@gmail.com>
2017-12-10 14:34:48 -0300

    Fix slick fonts and images?
    - Override slick images/fonts path
    - Disable filerev for images

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,7 +169,7 @@ module.exports = function (grunt) {
       dist: {
         src: [
           '<%= yeoman.dist %>/styles/**/*.css',
-          '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
+          // '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
           // '<%= yeoman.dist %>/styles/fonts/**/*.{woff,woff2,eot,svg,ttf}',
           '<%= yeoman.dist %>/views/**/*.html',
           '<%= yeoman.dist %>/scripts/**/*.js',
