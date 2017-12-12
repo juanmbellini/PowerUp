b37e3374f462e0305d94885fbf614d1baa0fa0a1
Juan Li Puma <juanlipuma94@gmail.com>
2017-12-11 21:12:57 -0300

    Reformat code
    - Reformat code
    - Delete unused dependencies
    - Use array declaration format for HomeCtrl

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,159 +1,152 @@
 'use strict';
-define(['powerUp', 'slick-carousel', 'onComplete', 'FeedService', 'LikesService'], function(powerUp) {
+define(['powerUp', 'slick-carousel', 'onComplete', 'FeedService', 'LikesService'], function (powerUp) {
 
-	powerUp.controller('HomeCtrl', function($scope, $location, Data, searchedTitleService, Restangular, SweetAlert, AuthService, FeedService, LikesService, $log, $route) {
-		Restangular.setFullResponse(true);
-		$scope.homePageText = 'This is your homepage';
-		$scope.data = Data;
-		$scope.gameTitle = '';
-		$scope.submitSearch = function() {
-			searchedTitleService.setTitle($scope.gameTitle);
-			$location.search({'name': $scope.gameTitle});
-			$location.path('search');
-      	};
+  powerUp.controller('HomeCtrl', ['$scope', '$location', '$log', '$route', 'searchedTitleService', 'Restangular', 'AuthService', 'FeedService', 'LikesService', function ($scope, $location, $log, $route, searchedTitleService, Restangular, AuthService, FeedService, LikesService) {
+    Restangular.setFullResponse(true);
+    $scope.homePageText = 'This is your homepage';
+    $scope.gameTitle = '';
+    $scope.submitSearch = function () {
+      searchedTitleService.setTitle($scope.gameTitle);
+      $location.search({'name': $scope.gameTitle});
+      $location.path('search');
+    };
 
-      	if (AuthService.isLoggedIn()) {
-			$scope.user = AuthService.getCurrentUser();
-			Restangular.all('users').one('username',$scope.user.username).get().then(function (response) {
-				var user = response.data;
-				Restangular.one('users',user.id).all('recommended-games').getList({}).then(function (response) {
-					$scope.recommendedGames = response.data;
-					$scope.recommendedMin = Math.min($scope.recommendedGames.length, 5);
-				});
-			});
-		}
+    if (AuthService.isLoggedIn()) {
+      $scope.user = AuthService.getCurrentUser();
+      Restangular.all('users').one('username', $scope.user.username).get().then(function (response) {
+        var user = response.data;
+        Restangular.one('users', user.id).all('recommended-games').getList({}).then(function (response) {
+          $scope.recommendedGames = response.data;
+          $scope.recommendedMin = Math.min($scope.recommendedGames.length, 5);
+        });
+      });
+    }
 
-		$scope.$on('recommendedRendered', function(event) {
-			angular.element('#recommended-carousel').slick({
-				infinite: false,
-				arrows: true
-			});
-			require(['lightbox2']); // TODO ensure requirejs doesn't load this twice
-		});
+    $scope.$on('recommendedRendered', function (event) {
+      angular.element('#recommended-carousel').slick({
+        infinite: false,
+        arrows: true
+      });
+      require(['lightbox2']); // TODO ensure requirejs doesn't load this twice
+    });
 
     /* ***********************************************************************
      *                                FEED
      * ***********************************************************************/
-		$scope.feed = [];
-		var feedObj;
-		if (AuthService.isLoggedIn()) {
-			feedObj = FeedService.initialize(AuthService.getCurrentUser().id);
-            $scope.loadingFeed = true;
-		}
+    $scope.feed = [];
+    var feedObj;
+    if (AuthService.isLoggedIn()) {
+      feedObj = FeedService.initialize(AuthService.getCurrentUser().id);
+      $scope.loadingFeed = true;
+    }
 
-        $scope.needFeed = function() {
-        	if (feedObj === null) {
-        		return null;
-        	}
-        	return FeedService.isReady(feedObj) + '' + $scope.loadingFeed;
-        };
-
-
-		$scope.$watch('needFeed()', function () {
-            if (AuthService.isLoggedIn() && feedObj !== null && FeedService.isReady(feedObj) && $scope.loadingFeed) {
-                var feedArray = FeedService.getFeed(feedObj);
-                feedArray.forEach(function (element, index, array) {
-                    $scope.feed.push(array[index]);
-                });
-                $scope.thereAreMore = feedObj.thereAreMore;
-                $scope.loadingFeed = false;
-            }
-		});
-
-        $scope.loadMoreFeed = function () {
-            $scope.loadingFeed = true;
-            // if (AuthService.isLoggedIn() && feedObj !== null) {
-            //     while ($scope.feedNeeded > 0 && FeedService.isReady(feedObj)) {
-            //         $scope.feedNeeded--;
-            //         var element = FeedService.getFeedElement(feedObj);
-            //         if (element !== null) {
-            //
-            //         }
-            //     }
-            // }
-        };
-        $scope.reloadFeed = function () {
-            $scope.feed = [];
-            if (AuthService.isLoggedIn()) {
-                feedObj = FeedService.initialize(AuthService.getCurrentUser().id);
-                $scope.loadingFeed = true;
-            }
-        };
+    $scope.needFeed = function () {
+      if (feedObj === null) {
+        return null;
+      }
+      return FeedService.isReady(feedObj) + '' + $scope.loadingFeed;
+    };
 
 
+    $scope.$watch('needFeed()', function () {
+      if (AuthService.isLoggedIn() && feedObj !== null && FeedService.isReady(feedObj) && $scope.loadingFeed) {
+        var feedArray = FeedService.getFeed(feedObj);
+        $scope.feed = $scope.feed.concat(feedArray);
+        // feedArray.forEach(function (element, index, array) {
+        //     $scope.feed.push(array[index]);
+        // });
+        $scope.thereAreMore = feedObj.thereAreMore;
+        $scope.loadingFeed = false;
+      }
+    });
+
+    $scope.loadMoreFeed = function () {
+      $scope.loadingFeed = true;
+      // if (AuthService.isLoggedIn() && feedObj !== null) {
+      //     while ($scope.feedNeeded > 0 && FeedService.isReady(feedObj)) {
+      //         $scope.feedNeeded--;
+      //         var element = FeedService.getFeedElement(feedObj);
+      //         if (element !== null) {
+      //
+      //         }
+      //     }
+      // }
+    };
+    $scope.reloadFeed = function () {
+      $scope.feed = [];
+      if (AuthService.isLoggedIn()) {
+        feedObj = FeedService.initialize(AuthService.getCurrentUser().id);
+        $scope.loadingFeed = true;
+      }
+    };
+
+    // Threads
+    $scope.isLikedByCurrentUser = function (thread) {
+      if (!$scope.isLoggedIn || !thread.hasOwnProperty('likedByCurrentUser')) {
+        return false;
+      }
+      return thread.likedByCurrentUser;
+    };
+
+    $scope.likeThread = function (thread) {
+      LikesService.like(Restangular.one('threads', thread.id), thread, function () {
+      }, function (error) {
+        $log.error('Error liking thread #', thread.id, ': ', error);
+      });
+    };
+
+    $scope.unlikeThread = function (thread) {
+      LikesService.unlike(Restangular.one('threads', thread.id), thread, function () {
+      }, function (error) {
+        $log.error('Error unliking thread #', thread.id, ': ', error);
+      });
+    };
+
+    // reviews
+    $scope.canDeleteReview = function (review) {
+      return AuthService.isLoggedIn() && AuthService.getCurrentUser().username === review.username;
+    };
+    $scope.deleteReview = function (review) {
+      review.remove().then(function (response) {
+          var data = response.data;
+          $log.info('Success: ', data);
+          $route.reload();
+          // TODO solo el filtering
+          // $scope.reviews = $scope.reviews.filter(function(reviewToFilter) {
+          //     return reviewToFilter.id !== review.id;
+          // });
+        },
+        function (error) {
+          $log.error('Error: ', error);
+        }, function () {
+          // $scope.checkCanWriteReview();
+        });
+    };
 
 
-
-		// Threads
-        $scope.isLikedByCurrentUser = function(thread) {
-            if (!$scope.isLoggedIn || !thread.hasOwnProperty('likedByCurrentUser')) {
-                return false;
-            }
-            return thread.likedByCurrentUser;
-        };
-
-        $scope.likeThread = function(thread) {
-            LikesService.like(Restangular.one('threads',thread.id), thread, function() {
-            }, function(error) {
-                $log.error('Error liking thread #', thread.id, ': ', error);
-            });
-        };
-
-        $scope.unlikeThread = function(thread) {
-            LikesService.unlike(Restangular.one('threads',thread.id), thread, function() {
-            }, function(error) {
-                $log.error('Error unliking thread #', thread.id, ': ', error);
-            });
-        };
-
-        // reviews
-        $scope.canDeleteReview = function(review) {
-            return AuthService.isLoggedIn() && AuthService.getCurrentUser().username === review.username;
-        };
-        $scope.deleteReview = function(review) {
-            review.remove().then(function(response) {
-                    var data = response.data;
-                    $log.info('Success: ', data);
-                    $route.reload();
-                // TODO solo el filtering
-                    // $scope.reviews = $scope.reviews.filter(function(reviewToFilter) {
-                    //     return reviewToFilter.id !== review.id;
-                    // });
-                },
-                function(error) {
-                    $log.error('Error: ', error);
-                },function () {
-                    // $scope.checkCanWriteReview();
-                });
-        };
-
-
-        // Follow friend
-        $scope.followFriend = function () {
-			if ($scope.friendName === $scope.user.username) {
-				$scope.followFriendError = true;
-				$scope.followFriendErrorMsj = 'You can\'t follow yourself!';
-				return;
-			}
-			$scope.followFriendErrorMsj = 'User doesn\'t exist';
-            $scope.loadingFollowFriend = true;
-            $scope.followFriendSend = true;
-            Restangular.all('users').one('username',$scope.friendName).get().then(function (response) {
-                var user = response.data;
-                Restangular.one('users', user.id).one('followers').put().then(function (response) {
-                    $scope.followFriendError = false;
-                    $scope.loadingFollowFriend = false;
-                }, function () {
-                    $scope.followFriendError = true;
-                    $scope.loadingFollowFriend = false;
-                });
-            }, function () {
-                $scope.followFriendError = true;
-                $scope.loadingFollowFriend = false;
-            });
-        };
-
-
-
-	});
+    // Follow friend
+    $scope.followFriend = function () {
+      if ($scope.friendName === $scope.user.username) {
+        $scope.followFriendError = true;
+        $scope.followFriendErrorMsj = 'You can\'t follow yourself!';
+        return;
+      }
+      $scope.followFriendErrorMsj = 'User doesn\'t exist';
+      $scope.loadingFollowFriend = true;
+      $scope.followFriendSend = true;
+      Restangular.all('users').one('username', $scope.friendName).get().then(function (response) {
+        var user = response.data;
+        Restangular.one('users', user.id).one('followers').put().then(function (response) {
+          $scope.followFriendError = false;
+          $scope.loadingFollowFriend = false;
+        }, function () {
+          $scope.followFriendError = true;
+          $scope.loadingFollowFriend = false;
+        });
+      }, function () {
+        $scope.followFriendError = true;
+        $scope.loadingFollowFriend = false;
+      });
+    };
+  }]);
 });
